a66ee19e2de6e5b8893befec72daedf24d5f4dce
narnav <narnavg@gmail.com>
2024-01-28 11:15:38 +0200

    first commit

---
+++ base/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+# Create your views here.
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+def index(req):
+    return Response('hello')
+
+# login
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        # Add custom claims
+        token['username'] = user.username
+        token['baga'] = 8888
+        # ...
+        return token
+
+
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getNotes(request):
+    return Response('private zone')
+
+
+@api_view(['GET'])
+def test(request):
+    return Response('public zone')
+
+
+# register
+@api_view(['POST'])
+def register(request):
+    user = User.objects.create_user(
+                username=request.data['username'],
+                email=request.data['email'],
+                password=request.data['password']
+            )
+    user.is_active = True
+    user.is_staff = True
+    user.save()
+    return Response("new user born")
